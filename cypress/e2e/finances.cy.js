c0d284ba78fc83705ae1f1c0a46e18f2adfe64ad
Leandro Santos <santosleandro1805@gmail.com>
2025-11-20 19:36:26 -0300

    Criando uma fuction de transação

--- cypress/e2e/finances.cy.js
+++ cypress/e2e/finances.cy.js
@@ -2,12 +2,18 @@ describe('Transações', ()=> {
     it('Cadastrar uma entrada', () => {
         cy.visit("https://dev-finance.netlify.app/")
 
-        cy.contains("Nova Transação").click()
-        cy.get('#description').type("Serviços Gerais")
-        cy.get('#amount').type(100)
+        criarTransacao("Serviços Gerais",100)
+        criarTransacao("Pintura",900)
+        
+    }); 
+});
+
+function criarTransacao(descricao, valor){
+    cy.contains("Nova Transação").click()
+        cy.get('#description').type(descricao)
+        cy.get('#amount').type(valor)
         cy.get('#date').type("2025-11-16")
 
         cy.contains('button', 'Salvar').click()
 
-    }); 
-});
+}
